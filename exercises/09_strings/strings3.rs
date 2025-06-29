8ba283550f077e8f7937949fc527baed680a2a39
vivek2584 <vivekroy2584@gmail.com>
2025-06-29 08:47:40 +0000

    soln. -> 09_strings

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,18 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut modified = input.to_string();
+    modified.extend(" world!".chars());
+    modified
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.to_string().replace("cars", "balloons")
 }
 
 fn main() {
