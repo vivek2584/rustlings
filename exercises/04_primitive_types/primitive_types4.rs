6dcd8a86f538287cef1403554a97c219cd9664da
vivek2584 <vivekroy2584@gmail.com>
2025-06-29 05:47:59 +0000

    soln. -> 04_primitive_types

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,8 @@
 fn main() {
     // You can optionally experiment here.
+    let a = [1, 2, 3, 4, 5];
+    let mut slice: [i32; 3] = [0; 3];
+    slice.clone_from_slice(&a[1..4]);
 }
 
 #[cfg(test)]
@@ -10,7 +13,7 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
-
+        let nice_slice: [i32; 3] = a[1..4].try_into().unwrap();
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
